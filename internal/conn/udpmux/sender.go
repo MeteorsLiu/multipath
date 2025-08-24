49a0b92c3d51234d7e33af8ae394db66c228fd53
Meteor <hello@liuxi.ng>
2025-08-24 08:55:25 +0000

    P

--- internal/conn/udpmux/sender.go
+++ internal/conn/udpmux/sender.go
@@ -32,11 +32,9 @@ func (u *udpSender) waitInPacket(bufs *[][]byte, pendingBuf *[]*mempool.Buffer) 
 	appendPacket := func(pkt *mempool.Buffer, packetType protocol.PacketType) {
 		protocol.MakeHeader(pkt, packetType)
 
-		size := pkt.Len()
 		pkt.OffsetTo(0)
-		pkt.SetLen(size + 1)
 
-		fmt.Println(pkt.Bytes())
+		fmt.Println("sent", pkt.Bytes())
 		*bufs = append(*bufs, pkt.Bytes())
 		*pendingBuf = append(*pendingBuf, pkt)
 	}
