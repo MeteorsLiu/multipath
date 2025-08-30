6979f5cf51186ec1d9b7746fb6d857fd0adb5142
Meteor <hello@liuxi.ng>
2025-08-30 12:08:49 +0000

    P

--- internal/prober/header.go
+++ internal/prober/header.go
@@ -10,21 +10,21 @@ import (
 
 const ProbeHeaderSize = 17
 
-func ProberIDFromBuffer(buf *mempool.Buffer) ([]byte, uuid.UUID, error) {
+func ProberIDFromBuffer(buf *mempool.Buffer) (byte, uuid.UUID, error) {
 	epoch := buf.Peek(1)
 	if epoch[0] > 0 {
-		return nil, uuid.Nil, fmt.Errorf("probe packet has been replied")
+		return 0, uuid.Nil, fmt.Errorf("probe packet has been replied")
 	}
-
 	id := buf.Peek(16)
 	proberId, err := uuid.FromBytes(id)
 	if err != nil {
-		return nil, uuid.Nil, fmt.Errorf("failed to parse probe packet header")
+		return 0, uuid.Nil, fmt.Errorf("failed to parse probe packet header")
 	}
-	return epoch, proberId, nil
+	return epoch[0], proberId, nil
 }
 
-func IncrEpoch(epoch []byte, pkt *mempool.Buffer) {
-	epoch[0]++
-	pkt.WriteAt(epoch, protocol.HeaderSize)
+func IncrEpoch(epoch byte, pkt *mempool.Buffer) {
+	var buf [1]byte
+	buf[0] = epoch
+	pkt.WriteAt(buf[:], protocol.HeaderSize)
 }
