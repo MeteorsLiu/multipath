bed89e5053f6c62c3bb82fd7f850b27ad08b5949
Haolan <hello@liuxi.ng>
2025-09-14 23:49:43 +0800

    fix: mtu size

--- internal/conn/header.go
+++ internal/conn/header.go
@@ -1,4 +1,4 @@
 package conn
 
-// 1500 - UDP Header Size(8B) - protocol Header Size(1B) - Reserved Header Size(1B)
-const MTUSize = 1490
+// 1500 - IP Header(20B) - UDP Header Size(8B) - protocol Header Size(1B) - Reserved Header Size(1B)
+const MTUSize = 1470
