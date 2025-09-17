1a74deb757aa0d43f4b735c4762e2d107ccd4505
Haolan <hello@liuxi.ng>
2025-09-17 23:47:35 +0800

    fix: path

--- internal/path/path.go
+++ internal/path/path.go
@@ -3,7 +3,6 @@ package path
 import (
 	"github.com/MeteorsLiu/multipath/internal/conn"
 	"github.com/MeteorsLiu/multipath/internal/mempool"
-	"github.com/MeteorsLiu/multipath/internal/prober"
 	"github.com/google/uuid"
 )
 
@@ -27,10 +26,6 @@ func (p *pathImpl) PathID() string {
 
 func (p *pathImpl) String() string {
 	return p.connSender.String()
-}
-
-func (p *pathImpl) Prober() *prober.Prober {
-	return p.connSender.Prober()
 }
 
 func (p *pathImpl) Write(b *mempool.Buffer) (err error) {
